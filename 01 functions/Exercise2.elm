aeb6834d32da2370c0216e7d6371c4a94ccae066
Ludovic Coullet <lcoullet@gmail.com>
2016-07-26 11:54:11 +0200

    exos finis

--- 01 functions/Exercise2.elm
+++ 01 functions/Exercise2.elm
@@ -1,3 +1,22 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import String
+
+
+capitalize : Int -> String -> String
+capitalize maxlength name =
+    if String.length name >= maxlength then
+        String.toUpper name
+    else
+        name
+
+main =
+  let
+  name = "Ludovic Coullet"
+  length = String.length name
+  in 
+  (capitalize 10 name)
+  ++ " - name length "
+  ++ toString length
+  |> Html.text
